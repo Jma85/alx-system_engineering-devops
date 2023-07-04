a39d637a3902833245a95dfa962e1d5960cdc1d2
Jma85 <jma24834@gmail.com>
2023-07-04 07:37:32 +0000

    3-redirection

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,27 +1,20 @@
-# Install and configure an Nginx server with the following requirements:
-# Listens on port 80
-# When querying Nginx at its root /, return a page containing the string Holberton School
-# Perform a permanent redirect when you query Nginx at /redirect_me
-
+# Install Nginx web server (w/ Puppet)
 package { 'nginx':
-      ensure => installed,
+  ensure => installed,
 }
 
-file_line { 'rewrite redirect':
-    ensure  => 'present',
-    path    => '/etc/nginx/sites-available/default',
-    after   => 'server_name _;',
-    line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-    require => Package['nginx'],
-    notify  => Service['nginx'],
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-file { '/var/www/html/index.nginx-debian.html':
-      content => 'Holberton School',
-      require => Package['nginx'],
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
 }
 
 service { 'nginx':
-    ensure  => 'running',
-    require => file_line['rewrite redirect'],
+  ensure  => running,
+  require => Package['nginx'],
 }
